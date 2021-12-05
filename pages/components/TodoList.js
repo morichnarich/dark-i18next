b006e979d349c8949a96334653be3b989bf83b61
morichnarich <morichnarich@gmail.com>
2021-12-05 18:50:09 +0900

    12/5 4

--- pages/components/TodoList.js
+++ pages/components/TodoList.js
@@ -11,41 +11,41 @@ import {
 import { FaTrash } from "react-icons/fa";
 
 function TodoList({ todos, deleteTodo }) {
-  // console.log(todos.length);
-  if (todos === []) {
+  console.log(todos.length);
+  if (todos.length === 0) {
     return (
       <Badge p="4" m="4" colorScheme="green" borderRadius="lg">
         NO TODOS, YAY!!
       </Badge>
     );
-  } else {
-    return (
-      <VStack
-        divider={<StackDivider />}
-        borderWidth="2px"
-        borderColor="grey.100"
-        borderRadius="lg"
-        p={4}
-        w="100%"
-        maxWidth={{ base: "90vw", sm: "80vw", lg: "70vw", xl: "40vw" }}
-        alignItems="stretch"
-      >
-        {todos.map((todo) => (
-          <HStack key={todo.id}>
-            <Text>{todo.body}</Text>
-            <Spacer />
-            <IconButton
-              icon={<FaTrash />}
-              isRound="true"
-              onClick={() => {
-                deleteTodo(todo.id);
-              }}
-            />
-          </HStack>
-        ))}
-      </VStack>
-    );
   }
+
+  return (
+    <VStack
+      divider={<StackDivider />}
+      borderWidth="2px"
+      borderColor="grey.100"
+      borderRadius="lg"
+      p={4}
+      w="100%"
+      maxWidth={{ base: "90vw", sm: "80vw", lg: "70vw", xl: "40vw" }}
+      alignItems="stretch"
+    >
+      {todos.map((todo) => (
+        <HStack key={todo.id}>
+          <Text>{todo.body}</Text>
+          <Spacer />
+          <IconButton
+            icon={<FaTrash />}
+            isRound="true"
+            onClick={() => {
+              deleteTodo(todo.id);
+            }}
+          />
+        </HStack>
+      ))}
+    </VStack>
+  );
 }
 
 export default TodoList;

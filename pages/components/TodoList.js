8bc62be54c10d0da48bc67cea2de774c2de41398
morichnarich <morichnarich@gmail.com>
2021-12-11 18:08:54 +0900

    12/12 2

--- pages/components/TodoList.js
+++ pages/components/TodoList.js
@@ -12,7 +12,7 @@ import { FaTrash } from "react-icons/fa";
 
 function TodoList({ todos, deleteTodo }) {
   console.log(todos);
-  if (todos.length === 0) {
+  if (todos[0] === undefined) {
     return (
       <Badge p="4" m="4" colorScheme="green" borderRadius="lg">
         NO TODOS, YAY!!
